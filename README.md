a7435981010143590e9b8fb81674fb66af8566c3
Hoerkley <Senai@CEPEAS21104204>
2024-06-26 21:19:51 -0300

    feat: Adicionado projeto MetalCoin e funcionalidade de cadastrar(ainda esta em teste).

--- README.md
+++
@@ -1,2 +0,0 @@
-# MetalCoin2.0
-Trabalho para desenvolver funcionalidades da API
---
+++ metalcoin-main/APRESENTATION/MetalCoin.Api/Controllers/ProdutoController.cs
@@ -0,0 +1,36 @@
+﻿using Metalcoin.Core.Domain;
+using Metalcoin.Core.Dtos.Request;
+using Metalcoin.Core.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetalCoin.Api.Controllers
+{
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoRepositories _produtoRepositories;
+
+        public ProdutoController(IProdutoRepositories produtoRepositories) {
+            _produtoRepositories = produtoRepositories;
+
+        }
+
+        [HttpGet]
+        [Route ("TodosProdutos")]
+        public async Task<ActionResult> ObterProduto() {
+            var listarProduto = await _produtoRepositories.ObterTodos();
+
+            if (listarProduto.Count == 0) {
+                return NoContent();
+            }
+            return Ok(listarProduto);
+        }
+
+        [HttpPost]
+        [Route ("CadastrarProdutos")]
+        public Task<Produto> CadastarProduto([FromBody] ProdutoCadastarRequest produto) {
+            
+
+        }
+    }
+}
